18e501caa54df84e4889f1623f2b0ea28424a7fd
LEXINFINTECH\aislingxie <aislingxie@lexinfintech.com>
2019-01-07 20:45:40 +0800

    add new comp

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,21 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
+import {
+    HomeWrapper,
+    HomeLeft,
+    HomeRight
+} from './style'
 
-export default ()=>{
-    return (<div>home</div>)
+class Home extends PureComponent{
+    render () {
+        return (<HomeWrapper>
+                    <HomeLeft>
+                        <img alt=""  className="pic" src ="//upload.jianshu.io/admin_banners/web_images/4600/67db00190e013279ccac4b00bc5702c5f974b9aa.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"/>
+                    </HomeLeft>
+                    <HomeRight>
+
+                    </HomeRight>
+                </HomeWrapper>)
+    }
 }
+
+export default Home
---
+++ src/pages/home/style.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const HomeWrapper =styled.div`
+    width:960px;
+    margin: 0 auto;
+    margin-top:30px;
+`
+export const HomeLeft = styled.div`
+    width: 640px
+    float:left;
+    padding-left:15px;
+    .pic{
+        width:100%;
+        height:270px;
+        padding-bottom:30px;
+        border-bottom:1px solid #ccc;
+    }
+`
+
+export const HomeRight = styled.div`
+    width: 280px;
+    float: right;
+`
